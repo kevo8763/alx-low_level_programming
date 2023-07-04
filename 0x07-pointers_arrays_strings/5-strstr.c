9f153b00f6664902dad98cf1ed15b44a425e35fa
kevo8763 <kipkirui8763@gmail.com>
2023-07-04 21:52:11 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - function
+ * @haystack: input
+ * @needle: input
+ * Return:  locates a substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *m = haystack;
+		char *n = needle;
+
+		while (*m == *n && *n != '\0')
+		{
+			m++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
