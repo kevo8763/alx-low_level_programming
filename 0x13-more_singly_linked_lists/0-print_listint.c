2c89fe5d4cc52a4e2439134f298c9c74517a4ffa
kevo8763 <kipkirui8763@gmail.com>
2023-08-01 07:20:07 +0000

    prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
+
 /**
- * print_listint - prints elements of a linked list
- * @h: linked list
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
@@ -14,6 +15,5 @@ size_t print_listint(const listint_t *h)
 		num++;
 		h = h->next;
 	}
-
 	return (num);
 }
